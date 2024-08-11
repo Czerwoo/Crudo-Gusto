9699ee4aa4dea1dcfe699891fdd407ac2320dc0f
Czerwoo <czerwoo@onet.pl>
2024-08-11 18:55:58 +0200

    corrected menu hambumburger

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,8 @@ export default function ContactPage() {
   return (
     <main className="">
       <Header content="Kontakt z naszymi delikatesami" />
-      <section className="flex max-sm:flex-col px-[20%] py-5 bg-beige-color">
-        <div className="max-sm:w-1/2 md:w-full">
+      <section className="flex flex-row max-sm:flex-col max-md:px-[5%] max-2xl:px-[10%] 2xl:px-[20%]   max-sm:text-base sm:text-sm md:text-sm lg:text-xl xl:text-2xl py-5 bg-beige-color py">
+        <div className="sm:w-1/2 max-sm:w-full max-sm:p-4">
           <h2>Crudo Gusto</h2>
           <p>
             Wiankowa 11A <br />
@@ -21,7 +21,7 @@ export default function ContactPage() {
             niedziela: <span className="text-red">nieczynne</span>
           </p>
         </div>
-        <div className="max-sm:w-1/2 sm:w-full">
+        <div className="sm:w-1/2 max-sm:w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2466.2730584369715!2d19.44582547699788!3d51.81944218785942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca65115eb8c7%3A0xab4fa6ae74ec2fff!2zV2lhbmtvd2EgMTFBLCA5MS00OTggxYHDs2TFug!5e0!3m2!1spl!2spl!4v1723150119668!5m2!1spl!2spl"
             className="border-0 w-full h-full"
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -15,31 +15,47 @@ export default function NavBar() {
     <div className="w-full h-30 flex items-center text-xl text-main-font-color font-main-font bg-beige-color max-md:px-[5%] max-2xl:px-[10%] 2xl:px-[20%] ">
       <div className="float-left h-full">
         <Link href="/">
-          <Image src={logo} width={240} height={120} alt="Logo" />
+          <Image
+            src={logo}
+            width={240}
+            height={120}
+            alt="Logo"
+            className={`${isOpen ? "hidden" : ""}`}
+          />
         </Link>
       </div>
-      <nav className="w-full h-full flex items-center  justify-end">
-        <button className="sm:hidden" onClick={toggleMenu}>
+      <nav className="w-full h-full flex items-center justify-end">
+        <button
+          className="sm:hidden h-full w-full flex justify-end"
+          onClick={toggleMenu}
+        >
           <FaBars size={32} />
         </button>
-        {/* Navigation Links */}
+
+        {/* Pełnoekranowe menu */}
         <div
           className={`sm:hidden ${
             isOpen
-              ? "w-full h-full relative flex items-center flex-col justify-center min-h-screen bg-gray-100 bg-white/90 p-8 rounded-lg shadow-lg z-10  "
+              ? "fixed inset-0 flex items-center justify-center bg-gray-100 bg-white/90 z-50"
               : "hidden"
-          } space-x-4`}
+          }`}
         >
-          <Link href="/">
-            <p>Nasze delikatesy</p>
-          </Link>
-          <Link href="/italian">
-            <p>Włoski stół</p>
-          </Link>
-          <Link href="/contact">
-            <p>Kontakt</p>
-          </Link>
+          <button className="absolute top-8 right-8" onClick={toggleMenu}>
+            <FaBars size={32} />
+          </button>
+          <div className="flex flex-col items-center space-y-8 text-2xl">
+            <Link href="/" onClick={toggleMenu}>
+              <p>Nasze delikatesy</p>
+            </Link>
+            <Link href="/italian" onClick={toggleMenu}>
+              <p>Włoski stół</p>
+            </Link>
+            <Link href="/contact" onClick={toggleMenu}>
+              <p>Kontakt</p>
+            </Link>
+          </div>
         </div>
+
         <div className="px-4 h-full max-sm:hidden">
           <ul className="flex gap-x-6">
             <li>
@@ -59,7 +75,6 @@ export default function NavBar() {
             </li>
           </ul>
         </div>
-        <div className="h-24 w-24 sm:hidden content-center"></div>
       </nav>
     </div>
   );
